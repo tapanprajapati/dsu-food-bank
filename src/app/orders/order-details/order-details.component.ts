dac253ede95177ab538303d74e39b83bbb6aaed5
Parth Parmar <parmar.parthsw@gmail.com>
2020-06-22 16:48:49 -0300

    merger-order-details

--- src/app/orders/order-details/order-details.component.ts
+++ src/app/orders/order-details/order-details.component.ts
@@ -1,4 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { OrderService } from './../order.service';
+import { OrderModel } from '@core/model/order.model';
+import { untilDestroyed } from '@app/@core';
 
 @Component({
   selector: 'app-order-details',
@@ -6,9 +11,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./order-details.component.scss'],
 })
 export class OrderDetailsComponent implements OnInit, OnDestroy {
-  constructor() {}
+  orderId: number;
+  order: OrderModel;
+  orderStatus = ['PLACED', 'APPROVED', 'PROCESSING', 'RECEIVED'];
 
-  ngOnInit() {}
+  constructor(private _route: ActivatedRoute, private _orderService: OrderService) {}
 
+  ngOnInit() {
+    this._observeOrderId();
+    this.order = this._getOrderDetails(this.orderId);
+  }
   ngOnDestroy() {}
+
+  private _observeOrderId() {
+    this._route.params.pipe(untilDestroyed(this)).subscribe((params) => {
+      this.orderId = +params.id;
+    });
+  }
+  private _getOrderDetails(orderId: number) {
+    return this._orderService.getOrderDetails(orderId);
+  }
 }
