7b71731c6e6018d045500106728e6ebc18e5fbf0
Parth Parmar <parmar.parthsw@gmail.com>
2020-06-22 20:41:36 -0300

    merged-forgot-password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,9 +7,31 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit, OnDestroy {
-  constructor() {}
+  resetPasswordForm!: FormGroup;
+  isResetPwdLinkSent = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this._resetPasswordForm();
+  }
 
   ngOnInit() {}
 
   ngOnDestroy() {}
+
+  resetPasswordLink() {
+    if (this.resetPasswordForm.valid) {
+      this.resetPasswordForm.reset();
+      this.isResetPwdLinkSent = true;
+    }
+  }
+
+  get email() {
+    return this.resetPasswordForm.controls.email;
+  }
+
+  private _resetPasswordForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 }
