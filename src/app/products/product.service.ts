d0152f3796441087c51fafdb9536b96db6328692
Parth Parmar <parmar.parthsw@gmail.com>
2020-07-21 18:51:25 -0300

    Product Image Loader

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,3 +1,4 @@
+import { ProductModel } from '@core/model/product.model';
 /**
  * @author Parth Parmar <parth.parmar@default.ca>
  *
@@ -7,6 +8,7 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { environment } from '@env/environment';
 
 import { GlobalErrorService } from '@core/services/global-error.service';
@@ -15,7 +17,11 @@ import { ApiResponseModel } from '@core/model/api-response.model';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  constructor(private _httpClient: HttpClient, private _globalErrorService: GlobalErrorService) {}
+  constructor(
+    private _httpClient: HttpClient,
+    private _globalErrorService: GlobalErrorService,
+    private _storage: AngularFireStorage
+  ) {}
 
   getAllProducts(params: any): Observable<ApiResponseModel> {
     return this._httpClient
@@ -32,6 +38,13 @@ export class ProductService {
   getProductDetails(id: string): Observable<ApiResponseModel> {
     return this._httpClient
       .get<ApiResponseModel>(`${this._getUrl()}/${id}`)
+      .pipe(catchError(this._globalErrorService.handleHttepResponseError));
+  }
+
+  fetchProductImage(productId: number): Observable<string | null> {
+    return this._storage
+      .ref(`images/${productId}`)
+      .getDownloadURL()
       .pipe(catchError(this._globalErrorService.handleHttepResponseError));
   }
 
