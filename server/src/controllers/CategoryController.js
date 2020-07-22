a3bf7cc9778a75e7a720b7a6249579099e3ec9f5
= <=>
2020-07-22 17:46:56 -0300

    API for adding and updating category created

--- server/src/controllers/CategoryController.js
+++ server/src/controllers/CategoryController.js
@@ -7,9 +7,21 @@
 function CategoryController(service) {
   this.service = service;
   this.getAll = this.getAll.bind(this);
+  this.create = this.create.bind(this);
+  this.update = this.update.bind(this);
 }
 CategoryController.prototype.getAll = async function getAll(req, res) {
   let response = await this.service.getAll();
+  res.status(response.statusCode).send(response);
+};
+
+CategoryController.prototype.create = async function create(req, res) {
+  let response = await this.service.create(req.body);
+  res.status(response.statusCode).send(response);
+};
+
+CategoryController.prototype.update = async function update(req, res) {
+  let response = await this.service.update(req.params, req.body);
   res.status(response.statusCode).send(response);
 };
 
