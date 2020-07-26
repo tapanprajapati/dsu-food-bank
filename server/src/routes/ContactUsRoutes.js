0fedde23587495b2d592fcb51a6deb03912179ec
ssshah <sm611862@dal.ca>
2020-07-25 22:12:33 -0300

    updated contactusroutes

--- server/src/routes/ContactUsRoutes.js
+++ server/src/routes/ContactUsRoutes.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 const { validate } = require('express-validation');
 
-const ContactUsService = require('src/services/ContactUSService');
+const ContactUsService = require('src/services/ContactUsService');
 const ContactUsController = require('src/controllers/ContactUsController');
 const contactUsController = new ContactUsController(new ContactUsService());
 const { authenticateRoute } = require('src/helpers/auth');
