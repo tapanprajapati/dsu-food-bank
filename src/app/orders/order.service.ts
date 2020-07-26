e1eedb74f6a89f8a5bcd2e77ef9d8c2ea039779c
Parth Parmar <parmar.parthsw@gmail.com>
2020-07-25 22:30:46 -0300

    Orders Module - baseUrl update

--- src/app/orders/order.service.ts
+++ src/app/orders/order.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment.prod';
 import { OrderDetailModel } from './../@core/model/order.model';
 import { Observable } from 'rxjs';
 import { OrderModel } from '@core/model/order.model';
@@ -12,15 +13,17 @@ export class OrderService {
   private orders: OrderModel[];
   private _globalErrorService: GlobalErrorService;
   private _storage: AngularFireStorage;
-  readonly URL = 'http://localhost:80/api/orders/';
   constructor(private http: HttpClient) {}
 
   getAllOrders(): Observable<OrderModel[]> {
-    console.log(this.http.get<OrderModel[]>(this.URL));
-    return this.http.get<OrderModel[]>(this.URL);
+    return this.http.get<OrderModel[]>(this._getUrl());
   }
 
   getOrderDetails(orderId: number) {
-    return this.http.get<OrderDetailModel[]>(this.URL + orderId);
+    return this.http.get<OrderDetailModel[]>(`${this._getUrl()}/${orderId}`);
+  }
+
+  private _getUrl() {
+    return `${environment.serverUrl}orders/`;
   }
 }
